e52ec4a39d6bda00b289b506e6ecec66ae88ea03
Drake Scott <dascott0204@gmail.com>
2017-05-05 00:38:55 -0500

    Type conversion and variables in C#.

---
+++ TypeConversion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 1;
+            int i = b;  //no data loss
+            Console.WriteLine(i);
+
+            //int z = 1000;
+            //byte b = i; //no workie. Cannot implicitly convert. Must use casting/explicit convert
+            //byte c = (byte) i;
+            //Console.WriteLine(c); //data loss
+
+            //Non compatible types
+            //var number = "1234";
+            //int Integer = Convert.ToInt32(number);
+            //Console.WriteLine(Integer);
+
+            try
+            {
+                string str = "true";
+                bool boon = Convert.ToBoolean(str);
+                Console.WriteLine(boon);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte");
+            }
+
+           
+        }
+    }
+}
