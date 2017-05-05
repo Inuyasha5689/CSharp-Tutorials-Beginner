a69a354d20557d700224fc13401c1a9d1e11412f
Drake Scott <dascott0204@gmail.com>
2017-05-05 01:37:38 -0500

    Classes in C#

---
+++ Classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classes.Math;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var drake = new Person();
+            drake.FirstName = "Drake";
+            drake.LastName = "Scott";
+            drake.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
