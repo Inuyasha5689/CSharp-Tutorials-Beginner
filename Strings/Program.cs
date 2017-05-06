685a01bcb586eabd02e5b2185956a67297461eaf
Drake Scott <dascott0204@gmail.com>
2017-05-06 01:41:32 -0500

    strings in C#

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -11,6 +11,27 @@ namespace Strings
         static void Main(string[] args)
         {
 
+
+            string firstName = "Drake"; //keyword
+            //var firstName = "Drake";
+            String lastName = "Scott"; //class
+            //var
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("My name is : {0} {1}", firstName, lastName);
+
+            var names = new string[3] {"john", "jack", "mary"};
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            //var text = "Hi Drake\nLook into the following path\nc:\\folder\\folder2\nc:\\folder3\\folder4";
+            var text = @"Hi Drake 
+Look into the following path
+c:\folder\folder2
+c:\folder3\folder4";
+            Console.WriteLine(text);
+
         }
     }
 }
