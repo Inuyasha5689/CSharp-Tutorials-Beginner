2b3a9798e070100b57fa8825da5d58f3eb3c4154
Drake Scott <dascott0204@gmail.com>
2017-05-07 18:52:30 -0500

    directory and directoryInfo classes in C# and .NET

---
+++ DirectoryandDirectoryInfo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryandDirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\myfile.jpg");
+//            var files = Directory.GetFiles(@"c:\projects\CSharpFundamentals", "*.sln", SearchOption.AllDirectories);
+//            foreach (var file in files)
+//                Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"c:\projects\CSharpFundamentals", "*.*",
+                SearchOption.AllDirectories);
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
