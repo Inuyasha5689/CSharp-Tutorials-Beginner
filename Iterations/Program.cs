007d46ab604523f782ceba0ff264683babb48015
Drake Scott <dascott0204@gmail.com>
2017-05-07 10:41:07 -0500

    foreach loops in C#

--- Iterations/Program.cs
+++ Iterations/Program.cs
@@ -10,20 +10,39 @@ namespace Iterations
     {
         static void Main(string[] args)
         {
-            for (var i = 1; i <= 10; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
+//            for (var i = 1; i <= 10; i++)
+//            {
+//                if (i % 2 == 0)
+//                {
+//                    Console.WriteLine(i);
+//                }
+//            }
+//
+//            for (var i = 10; i >= 1; i--)
+//            {
+//                if (i % 2 == 0)
+//                {
+//                    Console.WriteLine(i);
+//                }
+//            }
 
-            for (var i = 10; i >= 1; i--)
+//            var name = "Drake Scott";
+
+//            for (var i = 0; i < name.Length; i++)
+//            {
+//                Console.WriteLine(name[i]);
+//            }
+
+//            foreach (var character in name)
+//            {
+//                Console.WriteLine(character);
+//            }
+
+            var numbers = new int[] {1, 2, 3, 4};
+
+            foreach (var number in numbers)
             {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
+                Console.WriteLine(number);
             }
         }
     }
