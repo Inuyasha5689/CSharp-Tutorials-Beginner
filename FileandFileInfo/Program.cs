972b50ab7bef72a8ee67ac76a019eb4f0e5145f8
Drake Scott <dascott0204@gmail.com>
2017-05-07 18:45:55 -0500

    File and FileInfo classes.

---
+++ FileandFileInfo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileandFileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\someefile.jpg";
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                
+            }
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+
+            fileInfo.CopyTo("...", true);
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                
+            }
+//            fileInfo.Read doesn't exist.
+        }
+    }
+}
