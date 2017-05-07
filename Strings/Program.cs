b5527c134e2fcc35467038e8f78385d9630087c4
Drake Scott <dascott0204@gmail.com>
2017-05-07 17:35:51 -0500

    Part 2 of strings in C# and .Net

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -12,26 +12,54 @@ namespace Strings
         {
 
 
-            string firstName = "Drake"; //keyword
-            //var firstName = "Drake";
-            String lastName = "Scott"; //class
-            //var
+//            string firstName = "Drake"; //keyword
+//            //var firstName = "Drake";
+//            String lastName = "Scott"; //class
+//            //var
+//
+//            var fullName = firstName + " " + lastName;
+//
+//            var myFullName = string.Format("My name is : {0} {1}", firstName, lastName);
+//
+//            var names = new string[3] {"john", "jack", "mary"};
+//            var formattedNames = string.Join(",", names);
+//            Console.WriteLine(formattedNames);
+//
+//            //var text = "Hi Drake\nLook into the following path\nc:\\folder\\folder2\nc:\\folder3\\folder4";
+//            var text = @"Hi Drake 
+//Look into the following path
+//c:\folder\folder2
+//c:\folder3\folder4";
+//            Console.WriteLine(text);
 
-            var fullName = firstName + " " + lastName;
+            //String PART 2
+            var fullName = "Drake Scott ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
+            Console.WriteLine("ToLower: '{0}'", fullName.Trim().ToLower());
 
-            var myFullName = string.Format("My name is : {0} {1}", firstName, lastName);
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("FirstName: " + firstName);
+            Console.WriteLine("LastName: " + lastName);
 
-            var names = new string[3] {"john", "jack", "mary"};
-            var formattedNames = string.Join(",", names);
-            Console.WriteLine(formattedNames);
+            var names = fullName.Split(' ');
+            Console.WriteLine("FirstName: " + names[0]);
+            Console.WriteLine("lastName: " + names[1]);
 
-            //var text = "Hi Drake\nLook into the following path\nc:\\folder\\folder2\nc:\\folder3\\folder4";
-            var text = @"Hi Drake 
-Look into the following path
-c:\folder\folder2
-c:\folder3\folder4";
-            Console.WriteLine(text);
+            Console.WriteLine(fullName.Replace("Drake", "Dragon"));
+            
+            if (String.IsNullOrWhiteSpace(" "))
+                Console.WriteLine("Invalid");
 
+            var str = "25";
+            var age = Convert.ToByte(str);
+            Console.WriteLine(age);
+
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C0"));
+            
         }
     }
 }
